c39e2df854f96ea959ee19f7bbb4a5274069574d
ahmed-x86 <ahmdansmr7@gmail.com>
2026-02-07 11:52:26 +0200

    Add DaVinci Resolve conversion script

--- to_davinci.sh
+++ to_davinci.sh
@@ -8,7 +8,15 @@ fi
 INPUT="$1"
 OUTPUT="${INPUT%.*}_davinci.mov"
 
+# إشعار ببدء التحويل
+notify-send "Video Converter" "Starting conversion for $INPUT..." -i video-x-generic
 
+# التحويل
 ffmpeg -i "$INPUT" -c:v dnxhd -profile:v dnxhr_sq -pix_fmt yuv422p -c:a pcm_s16le "$OUTPUT"
 
-echo "تم التحويل بنجاح: $OUTPUT"
+# التحقق من نجاح العملية وإرسال الإشعار المناسب
+if [ $? -eq 0 ]; then
+    notify-send "Success" "Conversion finished: $OUTPUT" -i dialog-information
+else
+    notify-send "Error" "Conversion failed!" -i dialog-error
+fi
