75a9c188279508347bf687493d184e3f427751dd
ahmed-x86 <ahmdansmr7@gmail.com>
2026-04-20 18:01:47 +0200

    .

---
+++ to_mkv_nvenc.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+RED='\033[38;2;243;139;168m'
+GREEN='\033[38;2;166;227;161m'
+BLUE='\033[38;2;137;180;250m'
+YELLOW='\033[38;2;249;226;175m'
+NC='\033[0m'
+
+if ! command -v ffmpeg &> /dev/null; then
+    echo -e "${RED}❌ ffmpeg is not installed${NC}"
+    exit 1
+fi
+
+if [ $# -ge 1 ]; then
+    input_file="$1"
+else
+    echo -e "${BLUE}📂 Please enter the file path enclosed in quotes \" \":${NC}"
+    read -rp "> " input_file
+    
+    input_file="${input_file%\"}"
+    input_file="${input_file#\"}"
+    input_file="${input_file%\'}"
+    input_file="${input_file#\'}"
+fi
+
+if [ ! -f "$input_file" ]; then
+    echo -e "${RED}❌ File does not exist: $input_file${NC}"
+    exit 1
+fi
+
+filename=$(basename -- "$input_file")
+name="${filename%.*}"
+output_file="${name}.mkv"
+
+# تنبيه للمستخدم بنوع الكارت والحوسبة المستخدمة
+echo -e "${YELLOW}⏳ Converting to MKV using NVIDIA NVENC Hardware Acceleration...${NC}"
+
+# -hwaccel cuda: لفك التشفير بالكارت
+# -c:v h264_nvenc: للتشفير بالكارت
+ffmpeg -hwaccel cuda -hide_banner -loglevel error -stats -i "$input_file" -c:v h264_nvenc -c:a aac "$output_file"
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}✅ Conversion completed successfully: $output_file${NC}"
+else
+    echo -e "${RED}❌ Conversion failed (Make sure NVIDIA drivers and CUDA are installed)${NC}"
+fi
