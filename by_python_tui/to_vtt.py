b4fe79f7fc473b3d9df9967951fb052603420fc0
ahmed-x86 <ahmdansmr7@gmail.com>
2026-07-10 18:49:14 +0300

    Add/Update to_vtt.py

---
+++ by_python_tui/to_vtt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import os, curses
+from tui_base import check_dependencies, setup_catppuccin_colors, prompt_input, run_ffmpeg_ui
+
+def main_tui(stdscr):
+    curses.curs_set(0)
+    setup_catppuccin_colors()
+    
+    input_file = prompt_input(stdscr, "📝 Enter Subtitle File Path")
+    if not input_file or not os.path.isfile(input_file): return
+    
+    name, _ = os.path.splitext(input_file)
+    output_file = f"{name}.vtt"
+    
+    cmd = ["ffmpeg", "-y", "-i", input_file, output_file]
+    
+    run_ffmpeg_ui(stdscr, cmd, 0, 0, "Converting Subtitle to VTT Format...")
+
+if __name__ == "__main__":
+    check_dependencies()
+    curses.wrapper(main_tui)
